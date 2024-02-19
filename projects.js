a935748db091231d14c333a6612292c6e133f8bf
K.Shravya Shetty <21j14.shravya@sjec.ac.in>
2024-02-19 15:49:36 +0530

    deposit function created

---
+++ projects.js
@@ -0,0 +1,15 @@
+//1. depost some money
+//2.determine the number of lines to bet
+//3.collect a bet ammount
+//4.spin the slot machine
+//5.check if the user won
+//6.give the user their winnings 
+//7.play again
+
+const prompt=require("prompt-sync")();
+
+const deposit=()=>{
+    const depositAmount=prompt("enter a deposit amount : ")
+};
+
+deposit();
